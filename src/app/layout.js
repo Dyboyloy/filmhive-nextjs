b50f4f13103546093e2469dd170e12d6b41fd0a6
Dyboyloy <140319317+Dyboyloy@users.noreply.github.com>
2024-07-23 04:48:38 +0000

    Add Web Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Provider from "./Provider";
+import Analytics from "@/components/Analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Provider>
           <Header />
+          <Analytics />
           {children}
         </Provider>
       </body>
---
+++ src/components/Analytics.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Analytics } from "@vercel/analytics/react"
+
+export default function Analytics() {
+  return (
+    <div>
+        <Analytics />
+    </div>
+  )
+}
